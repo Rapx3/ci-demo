eb6b6b8f6d13dfe9274eb45bb57b9814663e17c3
Raphaël PINEAU <elfar.nop@gmail.com>
2018-03-09 18:47:16 +0100

    Added test

--- src/main/java/nc/indus/cidemo/controller/DogController.java
+++ src/main/java/nc/indus/cidemo/controller/DogController.java
@@ -3,7 +3,9 @@ package nc.indus.cidemo.controller;
 import nc.indus.cidemo.business.dto.Dog;
 import nc.indus.cidemo.business.services.DogService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/dogs")
 public class DogController {
 
-    @Autowired
-    DogService dogService;
+	@Autowired
+	DogService dogService;
 
-    @RequestMapping("/{name}")
-    Dog getDogByName(@RequestAttribute(name = "name") String name) {
-        return dogService.getDogByName(name);
-    }
+	@GetMapping("/{name}")
+	public ResponseEntity<Dog> getDogByName(@PathVariable("name") String name) {
+		return ResponseEntity.ok(dogService.getDogByName(name));
+	}
 
 }
